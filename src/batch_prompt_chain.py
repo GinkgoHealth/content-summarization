ea3d9098f24889d864c87a8e5c54e747fb33bf22
silvhua <silvhua@gmail.com>
2023-05-04 11:23:06 -0700

    replaced merge_chaining_results  with merge_all_chaining_results

--- src/batch_prompt_chain.py
+++ src/batch_prompt_chain.py
@@ -22,10 +22,10 @@ prep_step = [
 ]
 
 summarize_task = [
-    # "Summarize for a LinkedIn post",
-    # "Describe the interesting points to your coworker at the water cooler",
+    "Summarize for a LinkedIn post.",
+    "Describe the interesting points to your coworker at the water cooler",
     # "Create a Tiktok post",
-    "Create an Instagram post",
+    "Create an Instagram post without hashtags.",
     # "Summarize the article for a Tiktok post",
     # "Summarize for a tweet",
     # "Summarize in an engaging way",
@@ -46,18 +46,37 @@ simple_simplify_task = [
 
 user_simplify_task = [
     # "Use language appropriate for",
-    "If needed, rewrite using terms appropriate for",
+    # "If needed rewrite using terms appropriate for the audience. If not keep it the same. Your audience is",
+    """If needed, rewrite the text using terms appropriate for the audience. If not keep it the same.\
+    Follow these steps to accomplish this: \
+    \n1. Check if the content and language are appropriate for the audience. \
+    \n2. If it is suitable for the audience, keep it the same. If not, rewrite using terms appropriate for the audience. \ 
+    \n3. Return the final version of the summary to be shown to the audience. \
+    \n\nYour audience is""",
+    # "If needed, rewrite using terms appropriate for",
     # "Use terms a 12-year-old can understand.",
 ]
 
 simplify_audience = [
     # "",
-    "a lay audience",
-    # "people without a science background",
+    # "a lay audience",
+    "people without a science background",
 ]
 
 user_relevance_task = [
-    "Rewrite the summary to include an explanation of how it is relevant for",
+    # "Rewrite the summary to include an explanation of how it is relevant for",
+    # "Rewrite the summary to include a statement of how it is relevant for the audience"""
+    """Rewrite this summary to include a statement of how it is relevant for the audience. \
+        Follow these steps to accomplish this: \
+        \n1. Think about why this might be relevant for the audience in the grand scheme of things.\
+        \n2. If it is not evident why the text is relevant for the audience in the grand scheme of things, \
+        add a sentence to inform the audience. Otherwise, keep it the same. \
+        \n3. Modify the summary if needed to reduce redundancy. \
+        \n4. Check if the content and language are appropriate for the audience. \
+        If it is suitable for the audience, keep it the same. If not, rewrite using terms appropriate for the audience. \ 
+        \n5. Return the final version of the summary to be shown to the audience. \
+        \n6. Remove the backticks.
+        \n\nYour audience consists of""",
     # "Add 1-2 sentences to make this relevant for",
     # "Update the summary with 1-2 sentences to make it relevant for",
     # "Add 1-2 sentences to make this relevant for older adults."
@@ -67,13 +86,13 @@ user_relevance_task = [
 relevance_audience = [
     # "lay audience",
     # "",
-    # "seniors",
-    # "people who enjoy sports",
-    "people new to resistance training"
+    "seniors",
+    "people who enjoy sports",
+    # "people new to resistance training"
 ]
 
 # Create text dictionary
-folder_path = '../text/2023-05-02 2'
+folder_path = '../text/2023-05-04 2'
 encoding='ISO-8859-1'
 subset=None
 
@@ -90,6 +109,7 @@ qna_dict = dict()
 chatbot_dict = dict()
 simple_summaries_dict = dict()
 relevance_dict = dict()
+chain_results_dict = dict()
 
 # Create initial summaries
 qna_dict, chaining_dict = batch_summarize_chain(
@@ -116,9 +136,20 @@ relevance = prompt_chaining_dict(relevance_prompts, relevance_audience, relevanc
     )
 
 # Merge the results
-merged_df = merge_chaining_results(
-    qna_dict, chatbot_dict, 
-    simple_summaries_dict, relevance_dict, iteration_id, 
-    empty_columns=True, pivot=True, validate=True, 
-    chatbot_id=chatbot_id, save_df=True, save_chatbot=True,
+try:
+    chain_results_dict = merge_all_chaining_results(
+        chain_results_dict, chatbot_dict, iteration_id, pivot=True,
+        empty_columns=True,
+        save_df=True, save_chatbot=True, 
+            csv_path=folder_path,
     )
+    print(f'\nCompleted merge_all_chaining_results!:)')
+except:
+    merged_df = merge_chaining_results(
+        qna_dict, chatbot_dict, 
+        simple_summaries_dict, relevance_dict, iteration_id, 
+        empty_columns=True, pivot=True, validate=True, 
+        chatbot_id=chatbot_id, save_df=True, save_chatbot=True,
+        csv_path=folder_path, pickle_path=folder_path, json_path=folder_path
+        )
+    print(f'\nCompleted merge_chaining_results.')
