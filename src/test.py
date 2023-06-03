bb47fd0663583d4c7d4518ab6db45fb5f9b292d2
silvhua <silvhua@gmail.com>
2023-06-02 18:34:46 -0700

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,8 +1,12 @@
 import pandas as pd
+import os
+
+output_dir = '/app/content-summarization/src/outputs'  # Output directory within the container
 
 print('Creating test CSV...')
 df = pd.DataFrame()
 df['a'] = [1, 2, 3]
 
-df.to_csv('file')
-print('\tTest CSV saved')
+output_file = os.path.join(output_dir, 'file.csv')  # File path within the container
+df.to_csv(output_file)
+print('\tTest CSV saved to', output_file)
