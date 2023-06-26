7606ad06c35ed2df76f0090fef5d3a674f5959f0
silvhua <silvhua@gmail.com>
2023-06-26 13:43:28 -0700

    created scripts for connecting to db

--- src/db_session.py
+++ src/db_session.py
@@ -2,17 +2,19 @@ from sshtunnel import SSHTunnelForwarder
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from functools import wraps
+import sys
+sys.path.append(r"C:\Users\silvh\OneDrive\lighthouse\Ginkgo coding\content-summarization\private")
 
 # secrets.py contains credentials, etc.
-import secrets
+import db_secrets
 
 def get_engine_for_port(port):
     return create_engine('postgresql://{user}:{password}@{host}:{port}/{db}'.format(
-        user=secrets.pg_user,
-        password=secrets.pg_password,
+        user=db_secrets.pg_user,
+        password=db_secrets.pg_password,
         host='127.0.0.1',
         port=port,
-        db=secrets.db
+        db=db_secrets.db
     ))
 
 def with_sql_session(function, args, kwargs, engine=None):
@@ -32,10 +34,10 @@ def with_local_sql_session(function, *args, **kwargs):
 def with_remote_sql_session(function, *args, **kwargs):
     # Hat tip: https://stackoverflow.com/a/38001815
     with SSHTunnelForwarder(
-            (secrets.server_ip_address, 5995), # SH 2023-06-25 12:50 changed from 22 to 5995
-            ssh_username=secrets.ssh_username,
-            ssh_pkey=secrets.ssh_private_key_path,
-            ssh_private_key_password=secrets.ssh_private_key_password,
+            (db_secrets.server_ip_address, 5995), # SH 2023-06-25 12:50 changed from 22 to 5995
+            ssh_username=db_secrets.ssh_username,
+            ssh_pkey=db_secrets.ssh_private_key_path,
+            ssh_private_key_password=db_secrets.ssh_private_key_password,
             remote_bind_address=('127.0.0.1', 5432)
         ) as tunnel:
         tunnel.start()
