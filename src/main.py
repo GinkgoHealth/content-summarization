cd3853f4f0edd36ab595f05a46c32bf66fcbecf5
silvhua <silvhua@gmail.com>
2023-07-16 10:23:38 -0700

    test ability to print api key

---
+++ src/main.py
@@ -0,0 +1,8 @@
+
+from functools import wraps
+import sys
+sys.path.append(r"C:\Users\silvh\OneDrive\lighthouse\Ginkgo coding\content-summarization\private")
+import prompts
+
+print(type(prompts.api_key))
+print('hello')
