ce006a8649f0604366a8f5d0514635bb7a62b3d1
silvhua <silvhua@gmail.com>
2023-07-21 09:20:42 -0700

    fix mismatch between sources_df, gpt-queue

--- src/main.py
+++ src/main.py
@@ -1,16 +1,21 @@
-
 from functools import wraps
 import sys
 sys.path.append(r"C:\Users\silvh\OneDrive\lighthouse\Ginkgo coding\content-summarization\private")
+sys.path.append(r"C:\Users\silvh\OneDrive\lighthouse\Ginkgo coding\content-summarization\src")
 from prompts import * # .py file stored in the path above
-from db_orm import *
+from db_orm import * 
 from sources import *
 from orm_summarize import *
+from article_processing import *
 
 #########
+#########
 # Prep: Set parameters
+folder_path = '../text/2023-07-14 full'
+section = 'discussion'
+local = False
 n_choices = 1
-article_limit = 1
+article_limit = 4
 temperature = 1
 pause_per_request=0
 # summary_iteration_id = iteration_id
@@ -18,11 +23,9 @@ iteration_id = 1
 chatbot_id = iteration_id
 model = 'gpt-3.5-turbo-16k-0613'
 # model = 'gpt-4'
-save_outputs=True
-folder_path = '/database'
+save_outputs=False
 
-
-def generate_summaries(n_choices, temperature, model, pause_per_request, folder_path, article_limit=article_limit):
+def generate_summaries(n_choices, temperature, model, pause_per_request, folder_path, section, local, article_limit=article_limit):
     ### Set up
     qna_dict = dict()
     chatbot_dict = dict()
@@ -36,7 +39,10 @@ def generate_summaries(n_choices, temperature, model, pause_per_request, folder_
 
     ####### 
     # Step 1: Create sources table
-    text_df = get_table(table='gpt_queue', limit=article_limit) # db_orm.py
+    if local:
+        text_df = parse_fulltext(folder_path, section).iloc[:article_limit if article_limit else len(text_df)]
+    else:
+        text_df = get_table(table='gpt_queue', limit=article_limit) # db_orm.py
     references_df_dict[iteration_id] = create_sources_table(text_df) # sources.py
 
     ###### 
@@ -45,32 +51,30 @@ def generate_summaries(n_choices, temperature, model, pause_per_request, folder_
 
     # ##### 
     # Step 3: Get the new sources for summarization
-    sources_df = get_table(table='sources', limit=article_limit, order='DESC') # db_orm.py
+    sources_df = get_from_queue(input_df=text_df, order_by='id', order='ASC')
 
-    if sources_df['title'].tolist() == references_df_dict[iteration_id]['title'].tolist():
+    # ##### 
+    # Step 4: Create summaries (functions contained in orm_summarize.py)
+    chatbot_dict = batch_summarize( # orm_summarize.py
+        sources_df, folder_path, prep_step, summarize_task, edit_task,  # parameter values found in prompts.py
+        simplify_task, simplify_audience, format_task,
+        chatbot_dict, temperature=temperature,
+        system_role=system_role, model=model, max_tokens=1000,
+        n_choices=n_choices, pause_per_request=pause_per_request,
+        iteration_id=iteration_id, save_outputs=save_outputs
+        )
+    #########
+    # Step 5: Create summaries table
+    qna_dict = create_summaries_df(
+        qna_dict, chatbot_dict, iteration_id, chatbot_id=chatbot_id
+        )
 
-        # ##### 
-        # Step 3: Create summaries (functions contained in orm_summarize.py)
-        chatbot_dict = batch_summarize( # orm_summarize.py
-            sources_df, folder_path, prep_step, summarize_task, edit_task,  # parameter values found in prompts.py
-            simplify_task, simplify_audience, format_task,
-            chatbot_dict, temperature=temperature,
-            system_role=system_role, model=model, max_tokens=1000,
-            n_choices=n_choices, pause_per_request=pause_per_request,
-            iteration_id=iteration_id, save_outputs=save_outputs
-            )
-        #########
-        # Step 4: Create summaries table
-        qna_dict = create_summaries_df(
-            qna_dict, chatbot_dict, iteration_id, chatbot_id=chatbot_id
-            )
+    ##########
+    # Step 5: Add results to summaries and prompts table 
+    bulk_append(table='summaries', input_df=qna_dict[iteration_id]) # db_orm.py
 
-        ##########
-        # Step 5: Add results to summaries and prompts table 
-        bulk_append(table='summaries', input_df=qna_dict[iteration_id]) # db_orm.py
-
-        return qna_dict[iteration_id]
+    return qna_dict[iteration_id]
 
 if __name__ == "__main__":
-    qna_dict = generate_summaries(n_choices, temperature, model, pause_per_request, folder_path, article_limit=article_limit)
+    qna_dict = generate_summaries(n_choices, temperature, model, pause_per_request, folder_path, section, local=True, article_limit=article_limit)
     print(qna_dict[iteration_id])
