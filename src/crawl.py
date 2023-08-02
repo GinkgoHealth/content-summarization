fa2dc2706113274dbe8e9f33b1d25ce2e9f4120d
silvhua <silvhua@gmail.com>
2023-08-02 16:03:51 -0700

    from 2023-06-20 web crawling.ipynb

--- src/crawl.py
+++ src/crawl.py
@@ -1,56 +1,59 @@
 import scrapy
 from scrapy.crawler import CrawlerRunner
 from crochet import setup, wait_for
+import re
+from IPython import display
 import time
+from pprint import pprint
 
 setup()
-class spider_BMJO(scrapy.Spider):
-    name = "spider_BMJO"
-    
-    def start_requests(self):
-        urls = ['https://emails.bmj.com/q/1fnLH65XUsNn7Iiph6kELOM/wv']
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse_front)
-    
-    def parse_front(self, response):
-        self.article_title = response.css('a.art-title > font::text').extract()
-        article_url = response.xpath('//a[@class="art-title"]/@href').extract()
-        # self.article_title = [response.css('a.art-title > font::text').extract_first()]
-        # article_url = [response.xpath('//a[@class="art-title"]/@href').extract_first()]
-        for index, url in enumerate(article_url):
-            article_dict[index] = dict()
-            article_dict[index]['journal'] = 'BMJ Open'
-            article_dict[index]['title'] = self.article_title[index]
-            article_dict[index]['url'] = url
-            yield response.follow(url=url, callback=self.parse_pages, cb_kwargs={'index': index})
-    
-    def parse_pages(self, response, index):
-        text = response.xpath('//h2|//p|//h3|//h4').extract()
-        article_dict[index]['text'] = ''.join([line for line in text])
-        
-@wait_for(10)
-def run_spider():
-    """
-    Scrape articles from non-RSS URLs. Must instantiate a blank dictionary as `article_dict` 
-    before running the script.
-    
-    Parameters: None
 
-    How to call the function:
-    ```
-    article_dict = dict()
-    run_spider()
+def trim_text(text, regex=None):
+    if regex==None:
+        regex = '.*<h2>Abstract</h2>.*(?:Introduction.*)?(<h2.*?>Introduction</h2>.*References)<.*' 
+    try:
+        processed = re.search(regex, text, re.DOTALL).group(1)
+        html_display = display.HTML(processed)
+    except: 
+        print('Unable to parse article text')
+        processed = '<Error parsing article text>' 
+        html_display = processed
+    return processed, html_display
 
-    ```
+def text_dict_from_web(article_dict, header=2, to_display=0,
+        regex_str='.*<h\d>Abstract</h\d>.*(?:Introduction.*)?(<h\d.*?>Introduction</h\d>.*References)<.*'
+        ):
     """
-    crawler = CrawlerRunner()
-    d = crawler.crawl(spider_BMJO)
-    return d
+    Create a text dictionary from a dictionary containing web-scraped articles.
+
+    Parameters:
+        article_dict (dict): Values of each dictionary item are a dictionary representing the data from a 
+            single article: 'url', 'text', and 'title'.
+
+    Returns:
+        text_dict: Dictionary where each item is a string of the text of an article, starting with the title.
+    """
+    journal = next(iter(article_dict.values()))['journal']
+    print(f'Parsing {len(article_dict)} articles from {journal}')
+    regex_str = regex_str.replace('\d', f'{header}')
+    regex = rf'{regex_str}'
+    print(f'Regex pattern: {regex}')
+    text_dict = dict()
+    display_dict = dict()
+    if type(to_display) != list:
+        to_display = [to_display] 
+    for article_key in article_dict:
+        trimmed_text, display = trim_text(article_dict[article_key]['text'], regex)
+        text_dict[article_key] = f"{article_dict[article_key]['title']}\n\n{trimmed_text}"
+        if article_key in to_display:
+            display_dict[article_key] = display
+    print(f'text_dict keys: {[key for key in text_dict.keys()]}')
+    return text_dict, display_dict
 
 class crawler_RSS1(scrapy.Spider):
     name = "crawler_RSS1"
     
-    def __init__(self, n_articles='all'):
+    def __init__(self, n_articles):
         self.n_articles = n_articles
     
     def start_requests(self):
@@ -97,8 +100,8 @@ class crawler_RSS1(scrapy.Spider):
             [unique_article_title.append(article) for article in article_title if article not in unique_article_title]
             article_title = unique_article_title
             print(f'\tCorrected number of article titles: {len(article_title)}')
-        if type(self.n_articles) == int:
-            article_url = article_url[:self.n_articles]
+        if type(n_articles) == int:
+            article_url = article_url[:n_articles]
 
         for index, url in enumerate(article_url):
             # print(url)
@@ -147,6 +150,43 @@ def article_titles(article_dict):
     for article in sorted(article_dict):
         print(f"{article}: {article_dict[article]['title']}")
         print(f"\t{article_dict[article]['journal']} {article_dict[article]['url']}\n")
+
+def save_article_dict(article_dict, path, description='scraped_articles_dict', append_version=True,
+    save_pickle=True, save_json=False, to_csv=False):
+    """
+    Save a dictionary of articles to a file. Default behaviour is to save as a pickle only.
+    Parameters:
+        - article_dict (dict): Dictionary of articles.
+        - path (str): Path to save the file.
+        - description (str): Description of the file for the filename.
+        - append_version (bool): If True, append the date to the filename.
+        - save_pickle (bool): If True, save the dictionary as a pickle file.
+        - save_json (bool): If True, save the dictionary as a JSON file.
+        - to_csv (bool): If True, convert the dictionary to a DataFrame to save as a CSV file.
+    """
+    if save_pickle == True:
+        savepickle(article_dict, filename=f'{description}_', path=path, append_version=append_version)
+    if save_json == True:
+        save_to_json(article_dict, description=description, path=path, append_version=append_version)
+    if to_csv == True:
+        save_csv(pd.DataFrame(article_dict).transpose(), path=path, filename=f'{description}_',
+            index=False, append_version=append_version)
+
+
+iteration_id = 2.82
+# main_dict = dict()
+article_dict = dict()
+n_articles = 2
+run_RSS_spider(n_articles)
+
+main_dict[iteration_id] = article_dict
+# sorted(article_dict.keys())
+
+# save_article_dict(article_dict, path='../web_articles/2023-06-21', to_csv=True, save_json=True)
+article_titles(article_dict)
+
+# time.sleep(10)
+# text_dict, display_dict = text_dict_from_web(article_dict, to_display=[0])
 
 
 
